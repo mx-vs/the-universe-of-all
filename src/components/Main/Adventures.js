aab263c6f207bc99b264721b1297db3c7e8f00fa
mx-vs <mx.s.vasilev@gmail.com>
2021-04-06 18:47:42 +0300

    Styles - Adventure component

--- src/components/Main/Adventures.js
+++ src/components/Main/Adventures.js
@@ -19,35 +19,45 @@ class Adventures extends React.Component {
                         <li>Morina</li>
                         <li>Zorna</li>
                         <li>Libeto</li>
-                    </ul>
-                    <button className={styles.joinBtn}>Join Adventure</button>
-                </section>
-
-                {/* <section className={styles.adventure}>
-                    <img src="https://i.pinimg.com/564x/41/75/db/4175db1ce6579360b5ad91e0554ffa29.jpg" alt="" className="" />
-                    <h3>Miner Disaster</h3>
-                    <p>Location: Irian</p>
-                    <p>During regular mining work, your crew discovers a hidden city beneath the dust and dirt. Explore the city and its secrets.</p>
-                    <h4>Characters</h4>
-                    <ul>
-                        <li>Zepho</li>
-                        <li>Hosel</li>
-                        <li>Lisna</li>
+                        <button className={styles.joinBtn}>Join Adventure</button>
                     </ul>
                 </section>
 
                 <section className={styles.adventure}>
-                    <img src="https://i.pinimg.com/564x/02/b3/a0/02b3a099545ee5065fbd1fd41d5aaf0b.jpg" alt="" className="" />
-                    <h3>Thief Unknown</h3>
-                    <p>Location: Borliarde, Astarian</p>
-                    <p>During a grand ball hosted by House Ventruavian, a necklace belonging to the Duchess herself is stolen in a grand display of thievery. Uncover the thief.</p>
-                    <h4>Characters</h4>
-                    <ul>
+                    <section className={styles.imgWrapper}>
+                        <img src="https://i.pinimg.com/564x/41/75/db/4175db1ce6579360b5ad91e0554ffa29.jpg" alt="" className="" />
+                    </section>
+                    <section className={styles.textWrapper}>
+                        <h3>Miner Disaster</h3>
+                        <p>Location: Irian</p>
+                        <p>During regular mining work, your crew discovers a hidden city beneath the dust and dirt. Explore the city and its secrets.</p>
+                    </section>
+                    <ul className={styles.liWrapper}>
+                        <h4>Characters</h4>
+                        <li>Zepho</li>
+                        <li>Hosel</li>
+                        <li>Lisna</li>
+                        <button className={styles.joinBtn}>Join Adventure</button>
+                    </ul>
+                </section>
+
+                <section className={styles.adventure}>
+                    <section className={styles.imgWrapper}>
+                        <img src="https://i.pinimg.com/564x/02/b3/a0/02b3a099545ee5065fbd1fd41d5aaf0b.jpg" alt="" className="" />
+                    </section>
+                    <section className={styles.textWrapper}>
+                        <h3>Thief Unknown</h3>
+                        <p>Location: Borliarde, Astarian</p>
+                        <p>During a grand ball hosted by House Ventruavian, a necklace belonging to the Duchess herself is stolen in a grand display of thievery. Uncover the thief.</p>
+                    </section>
+                    <ul className={styles.liWrapper}>
+                        <h4>Characters</h4>
                         <li>Senga</li>
                         <li>Lida</li>
                         <li>Ronio</li>
+                        <button className={styles.joinBtn}>Join Adventure</button>
                     </ul>
-                </section> */}
+                </section>
             </div>
         )
     }
